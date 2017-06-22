a13036f3e03961c0f0bc4149d8d796107b990372
Ying Li <chemistryliying@gmail.com>
2017-06-22 10:57:03 -0700

    added collections of functions

---
+++ bootcamp_utils.py
@@ -0,0 +1,17 @@
+def ecdf(data):
+
+    x = sorted(data)
+    y = np.arange(1,len(data)+1)/len(data)
+    return x, y
+
+
+def gc_cont(seq):
+    # Initialize the GC counter
+    n_gc = 0
+
+    # Loop through the sequences and count G's and C's
+    for j, base in enumerate(seq):
+        if base in 'GCgc':
+            n_gc += 1
+
+    return(n_gc / len(seq))
